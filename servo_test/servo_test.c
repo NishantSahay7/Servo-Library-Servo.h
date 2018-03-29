bdc504c0d2987106753d36c1847a1024eb6170f0
Nishant Sahay <37246425+NishantSahay7@users.noreply.github.com>
2018-03-30 00:02:39 +0530

    Update servo_test.c

--- servo_test/servo_test.c
+++ servo_test/servo_test.c
@@ -16,8 +16,8 @@ int main()
 	set_degree(0);		//For 0 degree rotation
 	_delay_ms(1000);	//Delay for 1s
 	
-    set_degree(90);		//For 90 degree rotation
-    _delay_ms(1000);	//Delay for 1s
+  	set_degree(90);		//For 90 degree rotation
+  	_delay_ms(1000);	//Delay for 1s
 	
 	set_degree(180);	//For 180 degree rotation
 	_delay_ms(1000);	//Delay for 1s
