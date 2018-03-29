fe4ee6846d679ee7c10e0c83c2f033a8bb4745ca
Nishant Sahay <37246425+NishantSahay7@users.noreply.github.com>
2018-03-29 17:26:42 +0530

    Update servo.h

--- servo.h
+++ servo.h
@@ -6,11 +6,11 @@ servo_init()
 {
 	
 	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
-   TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM) extremeele
+  	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM) extremeele
 
-   ICR1=4999;  //fPWM=50Hz (Period = 20ms Standard).
+	ICR1=4999;  //fPWM=50Hz (Period = 20ms Standard).
 
-   DDRD|=(1<<PD4)|(1<<PD5);   //PWM Pins as Out
+  	DDRD|=(1<<PD4)|(1<<PD5);   //PWM Pins as Out
 }
 float set_degree(int d)
 {
